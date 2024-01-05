4d841e1e7468d09b41b0819f51083f431189e5ae
Tommy Johnsen <7oiden@gmail.com>
2024-01-05 10:01:50 +0100

    banner component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import "./sass/styles.scss";
-import Badge from "./components/Badge";
+import Badge from "./components/badge/Badge";
+import Banner from "./components/banner/Banner";
 
 function App() {
   return (
-    <Badge variant="square" color="purple">
-      This is my badge
-    </Badge>
+    <div className="wrapper">
+      <Badge variant="square" color="purple">
+        This is my badge
+      </Badge>
+      <Banner status="success" heading="Congratulations!">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+        pariatur, ipsum similique veniam.
+      </Banner>
+      <Banner status="error" heading="An error occurred"></Banner>
+      <Banner status="info" heading="Update available">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+        pariatur, ipsum similique veniam.
+      </Banner>
+    </div>
   );
 }
 
